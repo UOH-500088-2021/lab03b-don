6bef215f1a76733eb3cc6f2db9a61f5311f3888e
Louis Donaldson <louis.donaldson42@gmail.com>
2021-10-25 10:06:31 +0100

    Added max and min calls per year

--- DataProcessor/Program.cs
+++ DataProcessor/Program.cs
@@ -15,8 +15,10 @@ namespace DataProcessor
 
             Console.WriteLine(String.Format("Average total calls: {0}", dataReader.AverageTotalCalls));
             Console.WriteLine(String.Format("Number of rows: {0}", dataReader.NumberOfForces));
+            Console.WriteLine(String.Format("Maximum calls in a year: {0}", dataReader.MaxCallsInAYear));
+            Console.WriteLine(String.Format("Least calls in a year: {0}", dataReader.LeastCallsInAYear));
 
-            Console.WriteLine("Press enter key to exit");
+            Console.WriteLine("\nPress enter key to exit");
             Console.ReadLine();
         }
     }
@@ -28,6 +30,8 @@ namespace DataProcessor
         public int AverageTotalCalls { get; set; }
         public int NumberOfForces { get; set; }
         
+        public int MaxCallsInAYear { get; set; }
+        public int LeastCallsInAYear { get; set; }
 
         public void ReadData()
         {
@@ -50,12 +54,61 @@ namespace DataProcessor
                         continue;
                     }
                     PrintEachForceCalls(columns);
+                    MaxCalls(columns);
+                    LeastCalls(columns);
                 }
                 AverageTotalCalls = totalCalls / NumberOfForces;
             }
         }
 
-        
+        public void MaxCalls(string[] data)
+        {
+            // 12 columns per month
+            int totalCalls = 0;
+            for (int i = 1; i < 12; i++)
+            {
+                try
+                {
+                    totalCalls = +int.Parse(data[i]);
+                }
+                catch
+                {
+
+                }
+
+            }
+            if(MaxCallsInAYear < totalCalls)
+            {
+                MaxCallsInAYear = totalCalls;
+            }
+        }
+
+        public void LeastCalls(string[] data)
+        {
+            // 12 columns per month
+            int totalCalls = 0;
+            for (int i = 1; i < 12; i++)
+            {
+                try
+                {
+                    totalCalls = +int.Parse(data[i]);
+                }
+                catch
+                {
+
+                }
+
+            }
+            if (LeastCallsInAYear == 0)
+            {
+                LeastCallsInAYear = totalCalls;
+            }
+            if (LeastCallsInAYear > totalCalls)
+            {
+                
+                LeastCallsInAYear = totalCalls;
+            }
+        }
 
         public void PrintEachForceCalls(string[] data)
         {
